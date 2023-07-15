36ae757024336eb30e04811a8f4582557e94e547
Carla <crellian123@gmail.com>
2023-07-14 17:45:34 -0700

    Add padding, packing, random initial state

--- models/LSTM.py
+++ models/LSTM.py
@@ -1,41 +1,23 @@
-
-
-
-
-
-
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
 
 
 class LSTM(nn.Module):
-    def __init__(self, hidden_layers=64):
-        super(LSTM, self).__init__()
-        self.hidden_layers = hidden_layers
-        # lstm1, lstm2, linear are all layers in the network
-        self.lstm1 = nn.LSTMCell(1, self.hidden_layers)
-        self.lstm2 = nn.LSTMCell(self.hidden_layers, self.hidden_layers)
-        self.linear = nn.Linear(self.hidden_layers, 1)
-        
-    def forward(self, y, future_preds=0):
-        outputs, num_samples = [], y.size(0)
-        h_t = torch.zeros(n_samples, self.hidden_layers, dtype=torch.float32)
-        c_t = torch.zeros(n_samples, self.hidden_layers, dtype=torch.float32)
-        h_t2 = torch.zeros(n_samples, self.hidden_layers, dtype=torch.float32)
-        c_t2 = torch.zeros(n_samples, self.hidden_layers, dtype=torch.float32)
-        
-        for time_step in y.split(1, dim=1):
-            # N, 1
-            h_t, c_t = self.lstm1(input_t, (h_t, c_t)) # initial hidden and cell states
-            h_t2, c_t2 = self.lstm2(h_t, (h_t2, c_t2)) # new hidden and cell states
-            output = self.linear(h_t2) # output from the last FC layer
-            outputs.append(output)
-            
-        for i in range(future_preds):
-            # this only generates future predictions if we pass in future_preds>0
-            # mirrors the code above, using last output/prediction as input
-            h_t, c_t = self.lstm1(output, (h_t, c_t))
-            h_t2, c_t2 = self.lstm2(h_t, (h_t2, c_t2))
-            output = self.linear(h_t2)
-            outputs.append(output)
-        # transform list to tensor    
-        outputs = torch.cat(outputs, dim=1)
-        return outputs
+    def __init__(self, latent_size, action_size, hidden_size, batch_size, num_layers):
+        super().__init__()
+        self.h = Variable(torch.randn(num_layers, batch_size, hidden_size))
+        self.c = Variable(torch.randn(num_layers, batch_size, hidden_size))
+        self.lstm = nn.LSTM(latent_size + action_size, hidden_size, batch_first=True)
+
+    def forward(self, action, latent):
+        seq_lens = [a.size()[0] for a in action]
+        in_al = [torch.cat([a, l], dim=-1) for a, l in zip(action,latent)]
+        in_padded = torch.nn.utils.rnn.pad_sequence(in_al, batch_first=True)
+        in_packed = nn.utils.rnn.pack_padded_sequence(in_padded, seq_lens, batch_first=True, enforce_sorted=False)
+
+        outs, (self.h, self.c) = self.lstm(in_packed, (self.h, self.c))
+
+        unpacked, unpacked_len = torch.nn.utils.rnn.pad_packed_sequence(outs)
+
+        return unpacked
